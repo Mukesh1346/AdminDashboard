958f98995b4e2d4ec667b15e95d23bf7ec7c34f9
Mukesh1346 <mukeshmahar00@gmail.com>
2025-07-02 15:52:32 +0530

    first commit

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+import './dashboard.css'
+import { FaUsers, FaComments, FaUserCheck, FaImage, FaUserPlus, FaHeart } from 'react-icons/fa';
+
 
 export default function Dashboard() {
+  const [users, setuser] = useState(0);
+  const [messages, setMessages] = useState(10);
+  const [connections, setConnections] = useState(13);
+  const [subscriptions, setSubscriptions] = useState(11);
+  const [notifications, setNotifications] = useState(0);
+  const [reported , setReported] = useState(0);
+
+  const data = [
+    { label: 'Manage Users', value: users, icon: <FaUsers />, link: '/all-users' },
+    { label: 'Messages', value: messages, icon: <FaComments />, link: '/user-messages' },
+    { label: 'Match & Connections', value: connections, icon: <FaUserCheck />, link: '/verified-profiles' },
+    { label: 'Subscriptions & Payments', value: subscriptions, icon: <FaImage />, link: '/profile-photos' },
+    { label: 'Notifications', value: notifications, icon: <FaUserPlus />, link: '/new-signups' },
+    { label: 'Reported Profiles', value: reported, icon: <FaHeart />, link: '/all-matches' },
+  ];
+  
+
+
   return (
     <div style={{margin:"96px 0px 0px 261px", width:"82%"}}>
-      <h2 className='bg-warning'>Dashboard</h2>
+      <h2 className='text-center'>Admin Dashboard</h2>
+      <div className='dashboard-cards'>
+{  data.map((item,index)=>(
+  <Link to={item.link} key={index}  className='dashboard-card link-style'>
+    <div className='dashboard-icon'>{item.icon}</div>
+    <div className='text-light'>{item.label}</div>
+    <p>{item.value}</p>
+   
 
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde harum minima, cumque alias perspiciatis totam, quisquam odio aspernatur, architecto temporibus nesciunt consectetur? Hic, itaque quae porro esse enim aliquam ab quod inventore cumque laudantium laborum minus vero suscipit voluptatum quisquam. Corporis illum dolore quae non quisquam tenetur error dicta suscipit?</p>
+
+  </Link>
+))
+
+
+
+}
+
+      </div>
+
+      
     </div>
   )
 }
